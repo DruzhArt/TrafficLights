762377204b17c49024e992d9b18664981a9a72f0
DruzhArt <druzhart90@gmail.com>
2022-09-18 15:18:01 +0300

    setup labels and button

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+    }
+    
+    @IBAction func trafficButton() {
+    }
+    
     
 }
-
